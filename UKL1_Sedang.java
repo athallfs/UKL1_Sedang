131e4e3dad5f17c353d0d1baf9bde2d08a64c7f8
athallfs <atha.sahrizal@gmail.com>
2025-11-03 19:14:51 +0700

    Add files via upload

---
+++ UKL1_Sedang.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UKL1_Sedang {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("--- PROGRAM MENGHITUNG FAKTORIAL ---");
+        System.out.print("Masukkan bilangan bulat positif: ");
+        int angka = input.nextInt();
+
+        // Mengecek apakah bilangan valid (positif)
+        if (angka < 0) {
+            System.out.println("Bilangan harus positif!");
+        } else {
+            long faktorial = 1;
+
+            // Perulangan untuk menghitung faktorial
+            for (int i = 1; i <= angka; i++) {
+                faktorial *= i;
+            }
+
+            // Menampilkan hasil
+            System.out.println("\n--- HASIL PERHITUNGAN ---");
+            System.out.println("Bilangan : " + angka);
+            System.out.println("Faktorial: " + faktorial);
+        }
+
+        input.close();
+    }
+}
